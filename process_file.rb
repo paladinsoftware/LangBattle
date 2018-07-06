ddcea05dd5f033cf3e368e7c8c6232caa7c32f01
Milovan Zogovic <milovan.zogovic@gmail.com>
2018-07-06 20:16:35 +0200

    Ability to specify date range. Added reporting

--- process_file.rb
+++ process_file.rb
@@ -1,10 +1,20 @@
-filename = "files/#{ARGV[0]}"
+require 'pp'
+
+filename = ARGV[0]
+start_date = ARGV.fetch(1, '0000-00-00')
+end_date = ARGV.fetch(2, '9999-99-99')
+
 puts "Processing #{ARGV[0]}"
 
-result = {}
+result = Hash.new { |h, k| h[k] = 0 }
 File.foreach(filename) do |line|
-  id, views = line.split(',')
-  result[id.to_i] = result.fetch(id.to_i, 0) + views.to_i
+  day, video_id, views = line.split("\t")
+  next if day < start_date || day > end_date
+  result[video_id] += views.to_i
 end
 
-# puts result
+puts "TOP 5 VIDEOS:"
+pp result.max_by(5) { |k, v| v }
+
+memory = `ps -o rss -p #{Process::pid}`.chomp.split("\n").last.strip.to_i
+puts "MEMORY: #{memory/1024} MB"
