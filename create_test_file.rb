f90daece314a1c8f867eb2a2af0ac5dcd6605d4b
Milovan Zogovic <milovan.zogovic@gmail.com>
2018-07-06 20:31:37 +0200

    More realistic test files. Putting them in tmp folder to avoid backing up

--- create_test_file.rb
+++ create_test_file.rb
@@ -1,12 +1,18 @@
 # ruby create_test_file.rb test.txt 10
-# 0 - user id
-# 1 - views
+# 0 - day
+# 1 - video_id
+# 2 - views
+
+require 'date'
+require 'securerandom'
 
 filename = ARGV[0]
 iterations = ARGV[1].to_i
+video_ids = Array.new(100_000) { |n| SecureRandom.hex(6) }
+days = Array.new(60) { |n| (Date.today - n).to_s }
 
-file = File.open("files/#{filename}", 'w+')
-iterations.times do
-  file.write "#{rand(1...50)},#{rand(100..1000)}\r\n"
+File.open("tmp/#{filename}", 'w') do |file|
+  iterations.times do
+    file.write "#{days.sample}\t#{video_ids.sample}\t#{rand(100..1000)}\n"
+  end
 end
-file.close
